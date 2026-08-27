205991a46f40a4dbc2d345dbbb30bdecfe1d02fc
João Vitor <jvgaioskI@hotmail.com>
2026-08-27 17:18:05 -0300

    Search functionality

--- frontend/src/pages/Menu.tsx
+++ frontend/src/pages/Menu.tsx
@@ -9,11 +9,33 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 export default function Menu() {
   const [carregando, setCarregando] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
-  const [dadosPaginados, setDadosPaginados] =
-    useState<pageResponse<produto> | null>(null);
+  const [dadosPaginados, setDadosPaginados] = useState<pageResponse<produto> | null>(null);
 
   const busca = searchParams.get("busca") || "";
   const paginaAtual = Number(searchParams.get("pagina")) || 0;
+
+  const [termoBusca, setTermoBusca] = useState(busca);
+
+  /*
+  Esse useEffect é para quando é digitado algo na busca, ele atualiza inteligentemente e volta para a página "0"
+  mas caso esteja vazio ("") ele da o .delete para remover os valores.
+  */
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchParams((params) => {
+        if (termoBusca.trim()) {
+          params.set("busca", termoBusca);
+          params.set("pagina", "0");
+        } else {
+          params.delete("busca");
+        }
+        return params;
+      });
+    }, 500);
+
+    // Cacela o timer do atualizador de busca
+    return () => clearTimeout(timer);
+  }, [termoBusca, setSearchParams]);
 
   useEffect(() => {
     produtoService
@@ -34,7 +56,6 @@ export default function Menu() {
       params.set("pagina", novaPagina.toString());
       return params;
     });
-
     setTimeout(() => {
       window.scrollTo({
         top: 0,
@@ -57,18 +78,39 @@ export default function Menu() {
 
   return (
     <div className="min-h-screen p-6 flex flex-col justify-between max-w-7xl mx-auto w-full">
-        {/* Produtos */}
-        {produtos.length === 0 ? (
-          <p className="text-center text-stone-500 my-12">
-            Nenhum produto encontrado.
-          </p>
-        ) : (
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {produtos.map((produto) => (
-              <Product key={produto.id} produto={produto} />
-            ))}
-          </div>
-        )}
+      {/* Grid de Produtos e Busca */}
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {/* div de busca */}
+        <div className="sticky top-6 z-10 h-fit text-black bg-slate-100 border border-[#3f2b27]/10 rounded-2xl p-5 flex flex-col gap-2 shadow-sm">
+          <h2>Digite um produto</h2>
+          <input
+            type="text"
+            placeholder="Bolo de Cenoura..."
+            className="border border-[#3f2b27]/10 p-2 rounded-md"
+            onChange={(e) => {
+              setTermoBusca(e.target.value);
+            }}
+            value={termoBusca}
+          />
+          <button
+            className="bg-[#6b3d23] text-blue-50 p-2 rounded-md cursor-pointer"
+            onClick={() => {
+              setTermoBusca("");
+            }}
+          >
+            Voltar a página inicial
+          </button>
+        </div>
+        {/* map que renderiza os produtos */}
+        {produtos.map((produto) => (
+          <Product key={produto.id} produto={produto} />
+        ))}
+      </div>
+      {produtos.length === 0 && (
+        <p className="text-center text-stone-500 my-12">
+          Nenhum produto encontrado.
+        </p>
+      )}
 
       {/* Controle das páginas */}
       {dadosPaginados &&
