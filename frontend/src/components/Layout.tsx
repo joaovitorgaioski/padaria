d70e358d3126e9f3b9776b968e50c27ed0a9e95e
João Vitor <jvgaioskI@hotmail.com>
2026-09-01 13:06:29 -0300

    Unified header in Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,13 +1,36 @@
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import { Copyright } from "lucide-react";
+import { NavLink, Outlet } from "react-router-dom";
+import { Copyright, User } from "lucide-react";
+
+const navLinks = [
+  { label: "Home", path: "/" },
+  { label: "Cardápio", path: "/menu" },
+  { label: "Meus Pedidos", path: "/pedidos" },
+  { label: "Sobre Nós", path: "/sobre" },
+  { label: "Fale Conosco", path: "/contato" },
+];
 
 export default function Layout() {
   return (
     <div className="bg-[#6b3d23] flex flex-col text-blue-50 ">
-      <Header />
+      <header className="flex justify-between gap-10 p-5">
+        <h1>Backery</h1>
+        <nav className="flex gap-10">
+          {navLinks.map((item) => (
+            <NavLink
+              key={item.path}
+              to={item.path}
+              className={({ isActive }) =>
+                `text-slate-50 ${isActive && "font-bold"}`
+              }
+            >
+              {item.label}
+            </NavLink>
+          ))}
+        </nav>
+        <User />
+      </header>
 
-      <main className="flex-1 bg-[#f7efe6] h-screen">
+      <main className="flex-1 bg-[#f7efe6] h-screen no-scrollbar">
         <Outlet />
       </main>
 
